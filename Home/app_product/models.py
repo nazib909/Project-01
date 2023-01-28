b47fd03159df045ebcf19be3a6d313efe85a928e
Nazib <nazibhossen1@gmail.com>
2023-01-28 22:25:26 +0600

    New app for product category

---
+++ Home/app_product/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    c_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.c_name)
+
+
+class products(models.Model):
+    p_name = models.CharField(max_length=45)
+    price = models.FloatField()
+    p_price = models.FloatField()
+    decription = models.TextField(max_length=500)
+    quantity = models.PositiveIntegerField(default=1)
+    p_pic = models.ImageField(upload_to='product_pic', null=True)
+    catgory = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.p_name
---
+++ Home/app_product/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import renderfrom django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import userProfile
+from django.contrib.auth.models import User,auth
+
+def home(request):
+    return render(request, 'home.html')
